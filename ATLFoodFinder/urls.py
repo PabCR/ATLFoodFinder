6aa89f72a98b720cd62dbbc762dcaa9bdf8c6999
Akshar Ravichandran <145403803+AksharRavichandran@users.noreply.github.com>
2024-09-17 14:55:23 -0400

    Templates progress

--- ATLFoodFinder/urls.py
+++ ATLFoodFinder/urls.py
@@ -20,5 +20,6 @@ from ATLFoodFinder import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # Add the home URL path
+    path('', views.home, name='home'),
+    path('map/', views.map, name ='map'),  # Add the home URL path
 ]

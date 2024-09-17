103f8a2d4b984ee5bbc4626b0c17db23a9dc82c9
Rickbergl2 <rberglund7@gatech.edu>
2024-09-17 14:28:54 -0400

    Adding new pages for login, registration, lougout

--- ATLFoodFinder/urls.py
+++ ATLFoodFinder/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ATLFoodFinder import views 
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),  # Add the home URL path
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- ATLFoodFinder/views.py
+++ ATLFoodFinder/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 def home(request):
     return render(request, 'ATLFoodFinder/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redireect('home')
+    else:
+        form = UserCreationForm()

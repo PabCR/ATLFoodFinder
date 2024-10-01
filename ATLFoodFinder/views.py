1bde5085cd88f34ad3f973d147d3d3ff3eda1364
Rickbergl2 <rberglund7@gatech.edu>
2024-10-01 14:35:16 -0400

    Updated profile page

--- ATLFoodFinder/views.py
+++ ATLFoodFinder/views.py
@@ -7,11 +7,12 @@ from django.contrib.auth.views import PasswordResetView
 # views.py
 
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
 from django.contrib.auth.views import PasswordResetConfirmView
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, 'ATLFoodFinder/home.html')
@@ -39,4 +40,24 @@ class CustomPasswordResetConfirmView(PasswordResetConfirmView):
 
 @login_required
 def profile(request):
+    if request.method == 'POST':
+        user = request.user
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # Update the user's email (username)
+        if email and email != user.email:
+            user.email = email
+            user.username = email  # Also update the username
+            user.save()
+
+        # Update the user's password (if provided)
+        if password:
+            user.set_password(password)
+            user.save()
+            # Required to keep the user logged in after password change
+            update_session_auth_hash(request, user)
+
+        return redirect('profile')
+
     return render(request, 'ATLFoodFinder/profile.html')
